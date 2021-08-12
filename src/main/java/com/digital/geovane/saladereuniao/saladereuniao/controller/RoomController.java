bfa119397604e929ae75ef2b00cd7898ab7bcd47
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-08-11 23:52:54 -0300

    Desenvolvimento da class controller

--- src/main/java/com/digital/geovane/saladereuniao/saladereuniao/controller/RoomController.java
+++ src/main/java/com/digital/geovane/saladereuniao/saladereuniao/controller/RoomController.java
@@ -27,4 +27,51 @@ import javax.validation.Valid;
 public class RoomController {
 
     @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms(){
+        return roomRepository.findAll();
+    }
+
+    @GetMapping
+    public ResponseEntity<Room> getRoomById(@PathVariable(value = "id") long roomId)
+        throws ResourceNotFoundException{
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFoundException("Room Não encontrada: " + roomId));
+        return ResponseEntity.ok().body(room);
+    }
+
+    @PostMapping("/rooms")
+    public Room createRoom (@Valid @RequestBody Room room){
+        return roomRepository.save(room);
+    }
+
+    @PutMapping("/rooms/{id}")
+    public ResponseEntity<Room> updateRoom(@PathVariable (value = "id") Long roomId,
+                                           @Valid @RequestBody Room roomDetails ) throws ResourceNotFoundException{
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFoundException("Sala não encontrada com este id::" + roomId));
+        room.setName(roomDetails.getName());
+        room.setDate(roomDetails.getDate());
+        room.setStartHour(roomDetails.getStartHour());
+        room.setEndHour(roomDetails.getEndHour());
+        final Room updateRoom = roomRepository.save(room);
+        return ResponseEntity.ok(updateRoom);
+    }
+
+    public Map<String, Boolean> deleteRoom(@PathVariable(value = "id") Long roomId)
+        throws ResourceNotFoundException{
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(()-> new ResourceNotFoundException("Sala não encontrada com este id:"+ roomId));
+
+        roomRepository.delete(room);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+
+    }
+
+
+
 }
