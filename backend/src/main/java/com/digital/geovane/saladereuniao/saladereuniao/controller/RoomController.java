ff5f0a8572ee0c23a1df7fb0388c73684d45c512
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-08-13 21:46:04 -0300

    Finalização do backend

--- backend/src/main/java/com/digital/geovane/saladereuniao/saladereuniao/controller/RoomController.java
+++ backend/src/main/java/com/digital/geovane/saladereuniao/saladereuniao/controller/RoomController.java
@@ -34,7 +34,7 @@ public class RoomController {
         return roomRepository.findAll();
     }
 
-    @GetMapping
+    @GetMapping("/rooms/{id}")
     public ResponseEntity<Room> getRoomById(@PathVariable(value = "id") long roomId)
         throws ResourceNotFoundException{
         Room room = roomRepository.findById(roomId)
@@ -60,6 +60,7 @@ public class RoomController {
         return ResponseEntity.ok(updateRoom);
     }
 
+    @DeleteMapping("/rooms/{id}")
     public Map<String, Boolean> deleteRoom(@PathVariable(value = "id") Long roomId)
         throws ResourceNotFoundException{
         Room room = roomRepository.findById(roomId)
